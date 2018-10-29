529c9c411038b1f9bd7594d13a779f7e08185b43
John Thompson <john@springframework.guru>
2018-10-29 17:39:02 -0400

    removing junit 4

--- src/test/java/guru/springframework/domain/CategoryTest.java
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -4,7 +4,8 @@ package guru.springframework.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class CategoryTest {
 
