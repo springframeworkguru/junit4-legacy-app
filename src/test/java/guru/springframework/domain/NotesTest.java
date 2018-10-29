d80337e2c2765e8421d8641b71efd85b9dece14a
John Thompson <john@springframework.guru>
2018-10-29 17:22:04 -0400

    adding maven deps

---
+++ src/test/java/guru/springframework/domain/NotesTest.java
@@ -0,0 +1,18 @@
+package guru.springframework.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NotesTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getId() {
+        assertEquals(3, 3);
+    }
+}
