bb0f9c352af7130126e0283cc46ad3fae9c3fa7b
John Thompson <john@springframework.guru>
2018-10-29 16:50:45 -0400

    adding junit4 tests

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,15 @@
+package guru.springframework.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class IndexControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getIndexPage() {
+    }
+}
---
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -0,0 +1,15 @@
+package guru.springframework.services;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RecipeServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
